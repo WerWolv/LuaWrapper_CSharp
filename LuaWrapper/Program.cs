2691f2f844657c146d8c1d7c7374e078ba93a4f1
onepiecefreak3 <alberteinstein1980@gmx.de>
2018-08-30 22:44:17 +0200

    Several Context fixes; Updated test program;

--- LuaWrapper/Program.cs
+++ LuaWrapper/Program.cs
@@ -20,11 +20,17 @@ namespace LuaWrapper
             LuaContext context = new LuaContext();
             context.LoadFromFile("test.lua");
 
-            context.RegisterFunction("", "emptyRet", new Action<int>((int i) => { Console.WriteLine(i); }));
-            context.RegisterFunction("edizon", "oneRet", new Func<string, string>((string i) => { return i; }));
-            context.RegisterFunction("edizon", "multRet", new Func<string, int, (int, int)>(multRet));
+            //context.RegisterFunction("edizon", "emptyRet", new Action<int>((int i) => { Console.WriteLine(i); }));
+            //context.RegisterFunction("edizon", "oneRet", new Func<string, string>((string i) => { return i; }));
+            //context.RegisterFunction("edizon", "multRet", new Func<string, int, (int, int)>(multRet));
+            context.RegisterFunction("print", new Action<object>((object s) => {
+                Console.WriteLine("From C#: " + (s?.ToString() ?? "null"));
+            }));
+
+            context.RegisterFunction("edizon", "getStrArgs", new Func<string[]>(() => { return new string[] { "Test1", "Test2" }; }));
 
             context.Execute();
+            context.Execute("getValue");
 
             Console.ReadKey();
 
